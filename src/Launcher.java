cd468c3d72f1ce8c63997431f4b473d91a0e91b4
MaxPathak <67355216+MaxPathak@users.noreply.github.com>
2021-02-26 19:24:26 +0530

    commit

--- src/Launcher.java
+++ src/Launcher.java
@@ -2,11 +2,12 @@ package src;
 
 public class Launcher {
     public static void main(String[] args) {
-        //Initialize the Game object
+        // Initialize the Game object
         //Resolutions
         /*  1138 x 640 = 3 : 2
         */
         Game game = new Game("Banjara", 900, 600);
+        // Start the game
         game.start();
     }
 }
